96bbe3bb82cabce35d53ffae8087b72cfcb3e91a
Marta <martarut1994@gmail.com>
2018-05-29 19:25:53 +0200

    "Percent of green area"

--- src/utils/ColorDetection/ColorDetection.py
+++ src/utils/ColorDetection/ColorDetection.py
@@ -15,11 +15,19 @@ sensitivity = 20
 lower_range = np.array([36, 0, 0], dtype=np.uint8)
 upper_range = np.array([70, 255, 255], dtype=np.uint8)
 
-mask = cv2.inRange(hsv, (36 - sensitivity, 40, 0), (70 + sensitivity, 255, 255))
+mask = cv2.inRange(hsv, (36 - sensitivity, 50, 0), (70 + sensitivity, 255, 255))
 res = cv2.bitwise_and(img, img, mask=mask)
 
 cv2.imshow('res', res)
 cv2.imshow('image', img)
+
+grey = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
+height, width, channels = res.shape
+green = cv2.countNonZero(grey)
+pixels = grey.size
+percent_of_green_pixels = green / pixels * 100
+print("{:.2f}".format(percent_of_green_pixels))
 
 while (1):
     k = cv2.waitKey(0)
@@ -27,3 +35,4 @@ while (1):
         break
 
 cv2.destroyAllWindows()
+
